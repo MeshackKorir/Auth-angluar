4078b2432ed5ee6c3912eb06922de6f5c1d73b24
MeshackKorir <meshackkorir100@gmail.com>
2024-02-16 22:48:19 +0300

    login updater

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent,  FormsModule, RouterLink, CommonModule],
+  imports: [NavbarComponent, ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent {
-
-
+  loginforms!: FormGroup
+  constructor(private fb:FormBuilder){
+    this.loginforms = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+  router: any;
+  loginUser() {
+  console.log(this.loginforms.value);
+  
+}
 }
